98d1f998dede75648444e80d50a8006f1276c153
DawrAli33285 <lemightyeagle@gmail.com>
2025-09-22 11:52:45 -0700

    new

--- utils/multer.js
+++ utils/multer.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-const uploadDir = path.join(__dirname, '../uploads');
+const uploadDir = "/tmp/public/files/images"
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
